76bb2d8f5629521e46ad0f7c269b6905751cc283
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-06-20 11:01:58 +0200

    debugging and testing

--- model/water_management/main_water_management.py
+++ model/water_management/main_water_management.py
@@ -394,7 +394,7 @@ class WaterManagement(object):
             zonal_allocated_withdrawal_per_sector = {}
             for sector_name in self.sector_names:
                 zonal_allocated_withdrawal_per_sector[sector_name] = pcr.areatotal(cellAllocatedDemandPerSector[sector_name], allocation_zones)   
-                allocated_withdrawal_per_sector[sector_name] = totalVolCellWaterAbsraction * vos.getValDivZero(zonal_allocated_withdrawal_per_sector[sector_name], totalVolZoneAbstraction)
+                allocated_withdrawal_per_sector[sector_name] = totalVolCellWaterAbstraction * vos.getValDivZero(zonal_allocated_withdrawal_per_sector[sector_name], totalVolZoneAbstraction)
         
         return allocated_withdrawal_per_sector    
 
