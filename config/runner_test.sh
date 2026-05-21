5f1370a093e62a107abd741853b69348c6c781eb
Gabriel Cardenas Belleza <carde003@velocity.geo.uu.nl>
2026-05-21 18:06:52 +0200

    test run routing

--- config/runner_test.sh
+++ config/runner_test.sh
@@ -3,8 +3,8 @@
 #SBATCH -n 16
 #SBATCH -p rome
 #SBATCH -t 8:00:00
-##SBATCH -J kinematic
-#SBATCH -J accutravel
+#SBATCH -J kinematic
+##SBATCH -J accutravel
 #SBATCH --mail-type=END
 #SBATCH --mail-user=gcardenas1891@gmail.com
 
@@ -16,5 +16,5 @@ conda activate /gpfs/home6/gcardenas/.conda/envs/geo
 unset PCRASTER_NR_WORKER_THREADS
 export OPENBLAS_NUM_THREADS=1
 
-#python /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/model/deterministic_runner.py /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/config/setup_05min_2w_Rhine_kinematicwave.ini debug
-python /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/model/deterministic_runner.py /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/config/setup_05min_2w_Rhine_accutraveltime.ini debug
+python /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/model/deterministic_runner.py /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/config/setup_05min_2w_Rhine_kinematicwave.ini debug
+#python /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/model/deterministic_runner.py /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/config/setup_05min_2w_Rhine_accutraveltime.ini debug
