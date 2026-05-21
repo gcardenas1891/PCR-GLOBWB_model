54fdc26467410857de4a4dedc80fc31431c6961e
Gabriel Cardenas Belleza <carde003@velocity.geo.uu.nl>
2026-05-21 14:07:11 +0200

    test run routing

--- config/runner_test.sh
+++ config/runner_test.sh
@@ -2,8 +2,9 @@
 #SBATCH -N 1
 #SBATCH -n 16
 #SBATCH -p rome
-#SBATCH -t 120:00:00
-#SBATCH -J kinematic
+#SBATCH -t 8:00:00
+##SBATCH -J kinematic
+#SBATCH -J accutravel
 #SBATCH --mail-type=END
 #SBATCH --mail-user=gcardenas1891@gmail.com
 
@@ -15,5 +16,5 @@ conda activate /gpfs/home6/gcardenas/.conda/envs/geo
 unset PCRASTER_NR_WORKER_THREADS
 export OPENBLAS_NUM_THREADS=1
 
-python /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/model/deterministic_runner.py /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/config/setup_05min_2w_Rhine_kinematicwave.ini debug
+#python /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/model/deterministic_runner.py /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/config/setup_05min_2w_Rhine_kinematicwave.ini debug
 python /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/model/deterministic_runner.py /gpfs/home6/gcardenas/github/qualloc/PCR-GLOBWB_model/config/setup_05min_2w_Rhine_accutraveltime.ini debug
--- model/routing.py
+++ model/routing.py
@@ -1110,8 +1110,8 @@ class Routing(object):
         # - estimated based on the initial or latest sub-time step discharge (unit: m3/s)
         # 
         length_of_sub_time_step = pcr.ifthenelse(self.subDischarge > 0.0, 
-                                  self.water_height * self.dynamicFracWat * self.cellArea / \
-                                  self.subDischarge, vos.secondsPerDay())
+                                                 self.water_height * self.dynamicFracWat * self.cellArea / self.subDischarge, \
+                                                 vos.secondsPerDay())
         
         # determine the number of sub time steps (based on Rens van Beek's method)
         critical_condition = (length_of_sub_time_step < vos.secondsPerDay())  & \
