22e3725b325f8c83839185e870c70ee408f12b28
Gabriel Cardenas Belleza <carde003@velocity.geo.uu.nl>
2026-06-05 12:29:34 +0200

    testing routing

--- model/routing.py
+++ model/routing.py
@@ -1700,8 +1700,9 @@ class Routing(object):
             
             self.channelStorageTimeBefore = pcr.max(0.0, self.channelStorage)
             self.qualityLocal(meteo, landSurface, groundwater, currTimeStep)
-            pcr.aguila(self.waterTemp)   # deleteme!
-            pietje
+            pcr.aguila(self.waterTemp)             # <-------------- deleteme!
+            pcr.aguila(self.self.dynamicFracWat)   # <-------------- deleteme!
+            pietje                                 # <-------------- deleteme!
             self.qualityWaterBody()
         
         # ROUTING OPERATION:
